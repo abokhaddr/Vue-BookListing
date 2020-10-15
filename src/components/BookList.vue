a3e740233e060144edec5eb4c19de54b9d3b5c3f
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-10-15 20:41:23 +0300

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,19 +1,34 @@
  <template>
-  <li>
-    {{book.title}} : {{book.author}}
-  </li>
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for='book in books'>{{book.title}}:{{book.author}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'BookItem',
-  props: ['book'],
+  name: 'BookList',
+  data() {
+    return {
+      title: 'All Books',
+      books: [
+        {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+        {title: 'American Gods', author: 'Neil Gaiman'},
+        {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+      ]
+    };
+  }
 };
 </script>
 
 <style>
-li {
-  display: block;
-  margin: 0 10px;
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 </style> 
