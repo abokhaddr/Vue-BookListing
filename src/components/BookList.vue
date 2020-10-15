cb68cc0927c1ff47799efb8a31767ba1f987d893
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-10-15 20:15:23 +0300

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,22 +1,39 @@
-<template>
+ <template>
   <div>
     <h1>{{title}}</h1>
+    <ul>
+      <li v-for='book in books'>{{book.title}}:{{book.author}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import BookItem from './BookItem';
+import BookForm from './BookForm';
 export default {
   name: 'BookList',
   data() {
     return {
       title: 'All Books'
+      title: 'All Books',
+      books: [
+        {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+        {title: 'American Gods', author: 'Neil Gaiman'},
+        {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+      ]
     };
-  }
-};
-</script>
-
-<style>
+  },
+  components: {
+@@ -30,4 +38,12 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-</style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style> 
